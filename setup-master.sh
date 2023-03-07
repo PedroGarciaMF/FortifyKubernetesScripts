18d688ef50461c339542a8a4828b72240532c68c
pedrogarciamf <pedro.garcia@microfocus.com>
2023-03-07 11:07:49 -0500

    Adding microk8s.status

--- setup-master.sh
+++ setup-master.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Create fortify user
-sudo groupadd -r fortify && sudo useradd -m -r -g fortify fortify && sudo usermod -aG sudo fortify
-sudo su - fortify
 # Configure self-signed certificates
 sudo openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout /etc/ssl/private/master.key -out /etc/ssl/certs/master.pem -subj "/C=CA/ST=ON/L=Toronto/O=FortifyLab/OU=IT/CN=*.fortify.lab/emailAddress=fortify@fortify.lab"
 sudo apt-get update
 sudo apt-get install -y vim git openjdk-11-jdk dos2unix apt-transport-https net-tools network-manager haproxy
 # Configure microk8s
 sudo snap install microk8s --classic
-sudo usermod -a -G microk8s fortify
+microk8s.status
 microk8s.enable community
 microk8s.enable hostpath-storage
 microk8s.enable metallb:192.168.35.10-192.168.35.49
@@ -18,6 +15,7 @@ microk8s.enable helm3
 microk8s.enable rbac
 microk8s.enable registry
 microk8s.enable traefik
+microk8s.status
 # Patch kubeconfig
 microk8s.kubectl get nodes -o wide
 microk8s.config > ~/.kube/config
@@ -27,3 +25,6 @@ sudo snap install helm --classic
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add fortify https://fortify.github.io/helm3-charts
 helm repo update
+# Create fortify user
+sudo groupadd -r fortify && sudo useradd -m -r -g fortify fortify && sudo usermod -aG sudo fortify
+sudo usermod -a -G microk8s fortify
--- setup-worker.sh
+++ setup-worker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Create fortify user
-groupadd -r fortify && useradd -m -r -g fortify fortify && usermod -aG sudo fortify
-sudo su - fortify
+sudo apt-get update
 # Configure microk8s
 sudo snap install microk8s --classic
+microk8s.status
+# Create fortify user
+sudo groupadd -r fortify && sudo useradd -m -r -g fortify fortify && sudo usermod -aG sudo fortify
 sudo usermod -a -G microk8s fortify
